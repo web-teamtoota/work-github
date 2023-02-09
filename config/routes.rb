cbd9668db8c60fd9814c6e82aeed28a330ef2cad
tootaemi <n4mtkk@gmail.com>
2023-02-09 22:14:55 +0000

    [Update]制制作途中

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  
-  
   devise_for :users
-  root to: "homes#top"
+  get '/top' => 'homes#top'
+  
+  get "homes/about" => "homes#about", as: "about"
+  post 'homes' => 'homes#create'
+  resources :lists, only: [:new, :create, :index, :show, :edit, :destroy]
+    resources :users, only: [:show, :edit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
