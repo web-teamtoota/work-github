220bea8026908072f81c0f17a8aaca7adb95d29d
tootaemi <108526267+tootaemi@users.noreply.github.com>
2023-02-15 22:33:39 +0000

    [Update]商品新規登録画面投稿機能未完成

--- app/controllers/admins/items_controller.rb
+++ app/controllers/admins/items_controller.rb
@@ -3,9 +3,13 @@ class Admins::ItemsController < ApplicationController
   end
 
   def new
+      @item = Item.new
   end
 
   def create
+    item = Item.new(item_params)
+    item.save
+   redirect_to admin_items_path(@item.id)
   end
 
   def show
@@ -16,4 +20,10 @@ class Admins::ItemsController < ApplicationController
 
   def update
   end
+
+  private
+   def item_params
+    params.require(:item).permit(:profile_image, :id, :price, :sale, :faculty)
+   end
+
 end
