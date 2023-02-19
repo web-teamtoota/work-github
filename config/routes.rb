62fb25ab813921bf8a76cd44d777bac153cf705c
tootaemi <108526267+tootaemi@users.noreply.github.com>
2023-02-19 22:29:51 +0000

    [update]商品一覧画面途中

--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,26 @@
 Rails.application.routes.draw do
-  namespace :admins do
+  namespace :admin do
+    resources :items
+    end
 
-  end
-  devise_for :users
-  devise_for :admins
-  root to: 'homes#top'
+    devise_for :users
+    devise_for :admins
 
-
-  get "homes/about" => "homes#about", as: "about"
-  post 'homes' => 'homes#create'
-  devise_scope :admin do
+    root to: 'homes#top'
+    get "homes/about" => "homes#about", as: "about"
+    post 'homes' => 'homes#create'
     get "admin/sign_in", to: "admins/sessions#new"
+    get "admins" => "admins#index"
 
-    get 'admin/index', to: 'admins/items#index'
-    get 'admin/items/new', to: 'admins/items#new'
-    post 'admin/items' , to: 'admins/items#create'
-    get 'admin/items/:id', to: 'admins/items#show'
-    get 'admin/items/:id/edit', to: 'admins/items#edit'
-    patch 'admin/items/:id', to: 'admins/items#update'
-  end
+    #get 'admin/index', to: 'admins/items#index'
+    #get 'admin/items/new', to: 'admins/items#new'
+    #post 'admin/items' , to: 'admins/items#create'
+    #get 'admin/items/:id', to: 'admins/items#show'
+    #get 'admin/items/:id/edit', to: 'admins/items#edit'
+    #patch 'admin/items/:id', to: 'admins/items#update'
 
-
-  resources :lists, only: [:new, :create, :index, :show, :edit, :destroy]
-  resources :users, only: [:show, :edit, :update]
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
-
+    resources :lists, only: [:new, :create, :index, :show, :edit, :destroy]
+    resources :users, only: [:show, :edit, :update]
+    resources :admins, only: [:index, :new, :create, :show, :edit, :update]
+    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+    end
