3bbb7202f0ab7370da11cda2abad66f4bbdf1429
tootaemi <108526267+tootaemi@users.noreply.github.com>
2023-02-26 17:00:02 +0000

    [Update] 会員一覧画面,会員詳細画面, 会員情報編集画面, 管理者トップページ（注文履歴一覧画面）途中

---
+++ app/controllers/admin/customers_controller.rb
@@ -0,0 +1,21 @@
+class Admin::CustomersController < ApplicationController
+
+  def index
+    @customer = Customer.new
+    @customers = Customer.all.page(params[:page]).per(10)
+  end
+
+  def show
+    @customer = Customer.find(params[:id])
+  end
+
+  def edit
+    @customer = Customer.find(params[:id])
+  end
+
+  def update
+  end
+
+
+
+end
