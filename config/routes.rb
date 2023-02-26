65953c8b54ded15885dc3dc1b048be453c4a6dcc
tootaemi <108526267+tootaemi@users.noreply.github.com>
2023-02-26 22:13:52 +0000

    [Update] 管理者トップページ（注文履歴一覧画面）途中

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,10 @@ Rails.application.routes.draw do
   namespace :admin do
   resources :customers
   end
+  
+  namespace :admin do
+    resources :orders
+  end
 
     devise_for :users
     devise_for :admins
