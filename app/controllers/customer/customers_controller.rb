87d7a80e25212fd57c800af7fd08ccbf80b8dc21
tootaemi <n4mtkk@gmail.com>
2023-03-06 13:38:32 +0000

    [Update]新規会員登録画面_m途中

--- app/controllers/customer/customers_controller.rb
+++ app/controllers/customer/customers_controller.rb
@@ -1,10 +1,21 @@
-class Customer::CustomersController < ActionController
+class Customer::CustomersController < ApplicationController
   def index
-    @customer = Customer.all
+   
+      @customer = Customer.find(params[:id])
   end
   
    def new
+      @customer = Customer.find(params[:id])
     @customer = Customer.all
+   end
+   
+   def create
+     @customer = Customer.new(params[:id])
+   end
+  
+    private
+   def customer_params
+    params.require(:customer).permit(:last_name, :first_name)
    end
   
   
