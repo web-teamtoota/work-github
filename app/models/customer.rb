9d4e96f82999fb330e751e779acad1e76a38683b
tootaemi <n4mtkk@gmail.com>
2023-03-13 22:30:40 +0000

    [update]会員詳細画面未完成

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,14 +3,21 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  def full_name
+    self.last_name + " " + self.first_name
+  end
+  
 
+  attr_accessor :first_name, :last_name
 
- def full_name
+  def full_name
+    first_name.to_s + "" + last_name.to_s
+  end
 
-   self.last_name + " " + self.first_name
- end
-
-
-
-
+  def set(fname:, lname:)
+    self.first_name = fname # self.first_name =にする必要がある
+    self.last_name = lname # self.first_name =にする必要がある
+  end
 end
+
