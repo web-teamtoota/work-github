21cc39d736c70e0a435526bcd8b1c423981871e2
tootaemi <n4mtkk@gmail.com>
2023-03-22 21:41:14 +0000

    [Update]管理者トップページ（注文履歴一覧画面）,注文履歴詳細画面途中

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -32,7 +32,7 @@ class Admin::ItemsController < ApplicationController
   
     private
    def item_params
-    params.require(:item).permit(:image, :name, :introduction, :price)
+    params.require(:item).permit(:name, :introduction, :price, :is_active, :image)
    end
 
 
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,11 +1,23 @@
 class Admin::OrdersController < ApplicationController
 
+  def index
+     @order = Order.new
+  end
+
   def show
       @order = Order.find(params[:id])
   end
 
   def update
   end
+  
+  
+    private
+   def order_params
+    params.require(:order).permit(:item_id, :order_id, :quantity, :price, :making_status)
+   end
+
+  
   
 end
 
