4098cab99e60b66447d48df5c1e432fb384035fd
tootaemi <n4mtkk@gmail.com>
2023-03-26 09:31:30 +0000

    [Update]注文完了画面

--- config/routes.rb
+++ config/routes.rb
@@ -52,7 +52,15 @@ devise_for :customers,skip: [:passwords], controllers: {
   #get "customer/withdraw" => "customers#withdraw"
 
   resources :items
-  resources :orders
+  #resources :orders
+  get "orders/new" => "orders#new"
+  post "orders/confirm" => "orders#confirm"
+  get "orders/thanks" => "orders#thanks"
+  post "orders" => "orders#create"
+  get "orders" => "orders#index"
+  get "orders/:id" => "orders#show"
+  
+  
   #get "customer/sign_up" => "customers#new"
   #post "customer" => "customers#create"
   resources :cart_items
