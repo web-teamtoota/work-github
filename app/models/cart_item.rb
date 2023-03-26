5b4b287705132d104965edd51be74dcab0ea948c
tootaemi <n4mtkk@gmail.com>
2023-03-26 22:29:10 +0000

    [Update]カート画面途中0

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,2 +1,14 @@
 class CartItem < ApplicationRecord
+  
+  ## 消費税を求めるメソッド
+def with_tax_price
+    (price * 1.1).floor
+end
+
+## 小計を求めるメソッド
+def subtotal
+    item.with_tax_price * amount
+end
+
+
 end
