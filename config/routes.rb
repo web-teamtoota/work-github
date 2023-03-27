2c92320a0e2a35ecbe0f8d2a9d35a2d40114411c
tootaemi <n4mtkk@gmail.com>
2023-03-27 15:10:41 +0000

    [Update]カート機能途中2

--- config/routes.rb
+++ config/routes.rb
@@ -42,7 +42,7 @@ devise_for :customers,skip: [:passwords], controllers: {
   get "about" => "homes#about"
   get "customer/sign_up" => "customers#new"
   post "customer" => "customers#create"
-  
+
   get "customer" => "customers#show"
   get "customer/edit" => "customers#edit"
   patch "customer" => "customers#update"
@@ -59,11 +59,18 @@ devise_for :customers,skip: [:passwords], controllers: {
   post "orders" => "orders#create"
   get "orders" => "orders#index"
   get "orders/:id" => "orders#show"
-  
-  
+
+
   #get "customer/sign_up" => "customers#new"
   #post "customer" => "customers#create"
-  resources :cart_items
+
+  #resources :cart_items
+  get "cart_items" => "cart_items#index"
+  patch "cart_items/:id" => "cart_items#update"
+  delete "cart_items/:id" => "cart_items#destroy"
+  delete "cart_items" => "cart_items#destroy_all"
+  post "cart_items" => "cart_items#create"
+
   resources :addresses
   end
 
