c5678be02c255f3a28d70a6a85fea2491c023ea4
tootaemi <n4mtkk@gmail.com>
2023-05-07 12:32:53 +0000

    [Update]完成

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -44,6 +44,11 @@ class Public::RegistrationsController < Devise::RegistrationsController
    def configure_sign_up_params
      devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number])
    end
+   
+   def after_sign_in_path_for(resource)
+    #new_admin_session_path
+    customer_path
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_account_update_params
--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -66,5 +66,15 @@ end
       end
     end
   end
+  
+  def after_sign_in_path_for(resource)
+    #new_admin_session_path
+    root_path
+  end
 
+  def after_sign_out_path_for(resource)
+    #new_admin_session_path
+    root_path
+    
+  end
 end
