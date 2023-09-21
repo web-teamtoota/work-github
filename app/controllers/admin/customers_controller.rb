dc9b2a4cab7192bf35a13b7429d11145606947dd
tootaemi <n4mtkk@gmail.com>
2023-09-21 13:01:42 +0000

    [Update]コントローラーのインデント修正

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,28 +1,27 @@
 class Admin::CustomersController < ApplicationController
-    def index
-      @customers = Customer.all.page(params[:page]).per(10)
+  def index
+    @customers = Customer.all.page(params[:page]).per(10)
+  end
+  
+  def show
+    @customer = Customer.find(params[:id])
+  end
+  
+  def edit
+    @customer = Customer.find(params[:id])
+  end
+  
+  def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      redirect_to admin_customer_path
+    else
+      render :admin_customer_path
     end
-    
-    def show
-      @customer = Customer.find(params[:id])
-    end
-    
-    def edit
-      @customer = Customer.find(params[:id])
-    end
-    
-    def update
-      @customer = Customer.find(params[:id])
-      if @customer.update(customer_params)
-        redirect_to admin_customer_path
-      else
-        render :admin_customer_path
-      end
-    end
-    
+  end
+  
     private
     def customer_params
       params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :encrypted_password, :postal_code, :address, :telephone_number, :is_deleted)
     end
-
 end
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,55 +1,37 @@
 class Admin::OrdersController < ApplicationController
-
   before_action :authenticate_admin!
-
-
+  
   def index
     @orders = Order.all.page(params[:page]).per(10).order('created_at DESC')
   end
-
-
+  
   def show
     @order = Order.find(params[:id])
     @order_details = @order.order_details
   end
-
+  
   def update
     @order = Order.find(params[:id])
     @order.update(order_params)
     @order_details = @order.order_details
-
     if @order.status == "入金確認"
       @order_details.each do |order_detail|
         order_detail.making_status = "製作待ち"
         order_detail.save
-    end
+      end
     end
     redirect_to admin_order_path
-  
-
-  @order = Order.find(params[:id])
-  @order_details = OrderDetail.where(order_id: params[:id])
-  if @order.update(order_params)
-    @order_details.update_all(making_status: 1) if @order.status == "payment_confirmation"
+    @order = Order.find(params[:id])
+    @order_details = OrderDetail.where(order_id: params[:id])
+    if @order.update(order_params)
+      @order_details.update_all(making_status: 1) if @order.status == "payment_confirmation"
     ## ①注文ステータスが「入金確認」とき、製作ステータスを全て「製作待ち」に更新する
+    end
   end
-  # redirect_to admin_order_path
-end
-
-
-
-# private
-# def order_params
-#   params.require(:order).permit(:status)
-# end
-
   
   private
   def order_params
     params.require(:order).permit(:item_id, :order_id, :quantity, :price, :making_status, :postal_code, :address, :name, :total_payment, :postage,:payment_method, :status)
   end
-
-
+  
 end
-
-
